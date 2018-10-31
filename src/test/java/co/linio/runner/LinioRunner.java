b32813c002491c9f15626ad120125e0298a80a33
Diaz <ricardo.diaz@accenture.com>
2018-10-30 22:43:54 -0500

    Linio Runner

--- src/test/java/co/linio/runner/LinioRunner.java
+++ src/test/java/co/linio/runner/LinioRunner.java
@@ -5,7 +5,11 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.SnippetType;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
-
+/***7
+ * 
+ * @author ricardo.diaz
+ *
+ */
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
 		features = "src.test.resources/features/search_linio.feature",
